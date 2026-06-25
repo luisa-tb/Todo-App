2cbe636d61bd9556873612ac6fc1656ee030421e
Luisa Fernanda T <luisafernandatapiabarraza84@gmail.com>
2026-06-25 12:29:11 -0500

    comente una linra

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-app.options("*", cors());
+// app.options("*", cors());
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
